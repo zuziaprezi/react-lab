19ed214f0b96b6a3fbf1e8a812658666ea814e18
zwawro <zuzanna.wawro7@gmail.com>
2025-05-10 10:03:55 +0200

    E-mail and text field added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    let email = 'fracz@agh.edu.pl';
+
+    function handleChange(event) {
+        console.log(event.target.value);
+    }
+
+    return (
+      <div>
+
+        <h1>System do zapisów na zajęcia</h1>
+          <h2>Twój e-mail to {email}</h2>
+          <input type="text" onChange={handleChange}/>
+
+      </div>
+
   );
 }
 
