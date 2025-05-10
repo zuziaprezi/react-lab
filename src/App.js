176c4c3adbc6a8414a04b38d306e067b931d0faa
zwawro <zuzanna.wawro7@gmail.com>
2025-05-10 11:00:55 +0200

    Email lenght counter added.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState} from "react";
+
 function App() {
 
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState("example@example.com")
+
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
 
+    let message = '';
+    if (email.length <10){
+        message = "Twoj email jest za krótki";
+    } else if (email.length > 20) {
+        message ="Twoj email jest za dlugi"
+    } else {
+        message = "Twoj email jest w sam raz"
+    }
     return (
       <div>
 
         <h1>System do zapisów na zajęcia</h1>
-          <h2>Twój e-mail to {email}</h2>
+          <h2>Twój e-mail to: {email}</h2>
+          <div>{message}</div>
           <input type="text" onChange={handleChange}/>
 
       </div>
